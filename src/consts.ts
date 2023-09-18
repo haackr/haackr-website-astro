9ad16a6f0ea989864a6f18f5526720ad9bba6c25
Ryan Haack <ryan@haackr.dev>
2023-09-18 11:09:27 -0600

    some content/styling

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Ryan Haack | HAACKR LLC";
+export const SITE_DESCRIPTION =
+  "Ryan Haack | Full Stack Developer / Senior PMIS Consultant";
