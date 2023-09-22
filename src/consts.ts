5cf2b19c6dfb2f92c419478a81e8eac675070a2f
Ryan Haack <ryan@haackr.dev>
2023-09-22 17:23:49 -0600

    fix url

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = "Ryan Haack | HAACKR LLC";
 export const SITE_DESCRIPTION =
   "Ryan Haack | Full Stack Developer / Senior PMIS Consultant";
+export const BASE_URL = "https://haackr.dev";
