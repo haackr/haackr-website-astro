e809c981f2805b3bc7faddfe2d28d144a16ab36b
Ryan Haack <ryan@haackr.com>
2025-09-19 18:28:38 -0600

    change some of the site info

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Ryan Haack | HAACKR LLC";
-export const SITE_DESCRIPTION =
-  "Ryan Haack | Full Stack Developer / Senior PMIS Consultant";
-export const BASE_URL = "https://haackr.dev";
+export const SITE_TITLE = "Ryan Haack";
+export const SITE_DESCRIPTION = "Ryan Haack's personal website.";
+export const BASE_URL = "https://haackr.com";
