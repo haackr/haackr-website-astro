4cb472452241d20ac0f916a64f195437423a6c39
Ryan Haack <ryan@haackr.com>
2026-08-14 17:27:23 -0600

    Apparently I never saved the contact.ts file

--- functions/api/contact.ts
+++ functions/api/contact.ts
@@ -1,5 +1,70 @@
-interface Env {
+export interface Env {
   RESEND_API_KEY: string;
 }
 
-export const onRequestPost: PagesFunction = async ({ request, env }) => {};
+export const onRequestPost: PagesFunction<Env> = async ({
+  request,
+  env,
+}): Promise<Response> => {
+  try {
+    const formData = await request.formData();
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const message = formData.get("message");
+
+    if (!name || !email || !message) {
+      return new Response(
+        JSON.stringify({ error: "Missing required fields" }),
+        { status: 400, headers: { "Content-Type": "application/json" } },
+      );
+    }
+
+    const res = await fetch("https://api.resend.com/emails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${env.RESEND_API_KEY}`,
+      },
+      body: JSON.stringify({
+        from: "Ryan Haack <ryan@haackr.com>",
+        to: email as string,
+        reply_to: "ryan@haackr.com",
+        template: {
+          id: "contact-received",
+          variables: {
+            name: name as string,
+            sender_name: "Ryan",
+          },
+        },
+      }),
+    });
+
+    if (res.ok) {
+      return new Response(
+        JSON.stringify({
+          success: true,
+        }),
+        {
+          status: 200,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    } else {
+      return new Response(
+        JSON.stringify({ success: false, error: "Failed to Send Email" }),
+        {
+          status: 500,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    }
+  } catch (err) {
+    return new Response(
+      JSON.stringify({ success: false, error: "Internal Server Error" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+};
