83753175a090ccdcfcd707276a590cb15fa1544d
Ryan Haack <ryan@haackr.com>
2026-08-14 18:32:23 -0600

    Update astro to latest version and refactor.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://haackr.dev",
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [mdx(), sitemap(), react(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
